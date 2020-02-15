85e8638161e9273dd416e87b2e07c327525f262a
alex <alex@tekartik.com>
2020-02-15 17:54:20 +0100

    feat: dart2

--- audio_test/web/audio_test.dart
+++ audio_test/web/audio_test.dart
@@ -7,54 +7,59 @@ import 'dart:async';
 AudioContext audioContext;
 
 void main() {
-  audioContext = new AudioContext();
-  querySelector("#play_note")..onClick.listen(playNote);
-  querySelector("#play_file")..onClick.listen(playFile);
-  querySelector("#play_oscillator")..onClick.listen(playOscillator);
+  audioContext = AudioContext();
+  querySelector('#play_note')..onClick.listen(playNote);
+  querySelector('#play_file')..onClick.listen(playFile);
+  querySelector('#play_oscillator')..onClick.listen(playOscillator);
 }
 
-playFile(_) {
+Future playFile(_) {
   //GainNode gainNode = audioContext.createGain();
 
   // get the audio file
-  return HttpRequest.request("sample.ogg", responseType: "arraybuffer").then((HttpRequest request) {
+  return HttpRequest.request('sample.ogg', responseType: 'arraybuffer')
+      .then((HttpRequest request) {
     // decode it
-    return audioContext.decodeAudioData(request.response).then((AudioBuffer buffer) {
-      AudioBufferSourceNode source = audioContext.createBufferSource();
+    return audioContext
+        .decodeAudioData(request.response as ByteBuffer)
+        .then((AudioBuffer buffer) {
+      var source = audioContext.createBufferSource();
       source.buffer = buffer;
       source.connectNode(audioContext.destination);
       // play it now
       source.start(audioContext.currentTime);
     });
   });
-
 }
-playNote(_) {
+
+void playNote(_) {
   // create the buffer data from scratch
-  final int SAMPLE_RATE = 44100;
-  final double PI_2 = PI * 2;
-  final int BUFFER_SIZE = 4096;
-  AudioBuffer audioBuffer = audioContext.createBuffer(1, BUFFER_SIZE, SAMPLE_RATE);
+  final  SAMPLE_RATE = 44100;
+  final  PI_2 = pi * 2;
+  final  BUFFER_SIZE = 4096;
+  final audioBuffer =
+      audioContext.createBuffer(1, BUFFER_SIZE, SAMPLE_RATE);
 
-  Float32List buf = audioBuffer.getChannelData(0);
+  final buf = audioBuffer.getChannelData(0);
 
-  for (int i = 0; i < 4096; ++i) {
+  for (var i = 0; i < 4096; ++i) {
     buf[i] = sin(440 * PI_2 * i / SAMPLE_RATE);
   }
 
-  AudioBufferSourceNode source = audioContext.createBufferSource();
+  var source = audioContext.createBufferSource();
   source.buffer = audioBuffer;
   source.connectNode(audioContext.destination);
   // play it now
   source.start(audioContext.currentTime);
 }
 
-playOscillator(_) {
-  OscillatorNode oscillator = audioContext.createOscillator(); // Create sound source
+Future playOscillator(_) async {
+  var oscillator =
+      audioContext.createOscillator(); // Create sound source
 
   oscillator.connectNode(audioContext.destination); // Connect sound to output
-  oscillator.start(0); // Play instantly
-  new Future.delayed(new Duration(milliseconds: 1000), () {
+  oscillator.start2(0); // Play instantly
+  await Future.delayed(Duration(milliseconds: 1000), () {
     oscillator.disconnect(0);
   });
 }
